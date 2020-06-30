49316d8285bc21af7852f8a3d658ad0d82aadbb2
rflandau <rflandau@pm.me>
2020-06-30 12:34:35 -0700

    Started Command block and ScriptStrcut

--- src/Command.java
+++ src/Command.java
@@ -8,11 +8,19 @@ NOTE: each command block is unlikely to have more than one or two interpreters
 
 public class Command{
   //variables-------------------------------------------------------------------
-
+  // basic info
+  String id, name, tooltip;
+  int inputs; //may  need to be tweaked after we figure out how to take text
+  // hashtable stores K -> V as interpreter -> code
+  private Hashtable<String, String> codeTable = new Hashtable<String, String>();
   //constructors----------------------------------------------------------------
-  public Command(){
-
+  public Command(String id, String name){
+    this.id   = id;
+    this.name = name;
   }
   //subroutines-----------------------------------------------------------------
+  /* addLang
+    Adds an entry to the hashtable (interp -> code) */
+  void addLang(String interp, String code){codeTable.add(interp, code); return;}
   //static subroutines----------------------------------------------------------
 }
--- src/ScriptStruct.java
+++ src/ScriptStruct.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
 /* ScriptStruct
   Represents the main generation class for storing the flowchart and outputting
-  to the selected out-lang */
+  to the selected out-lang.
+  'Flow' holds references to the commands stored in the hashtable in main.
+  Declaration ("new" tag)should NOT be used with relation to flow or we will add
+    a significant amount of overhead. */
 
 public class ScriptStruct{
   //variables-------------------------------------------------------------------
-
+  private ArrayList<Command> flow = new ArrayList<Command>();
   //constructors----------------------------------------------------------------
   public ScriptStruct(){
 
   }
   //subroutines-----------------------------------------------------------------
+  void insertCommand(Command c, int i){
+
+  }
   //static subroutines----------------------------------------------------------
 }
