1fa0b4c6aad9211b34ca9d1a038b2853c652d024
rflandau <rflandau@pm.me>
2020-06-30 13:15:38 -0700

    Added getters and setters to Command as well as addLang.

--- src/Command.java
+++ src/Command.java
@@ -9,7 +9,7 @@ NOTE: each command block is unlikely to have more than one or two interpreters
 public class Command{
   //variables-------------------------------------------------------------------
   // basic info
-  String id, name, tooltip;
+  private String id, name, tooltip;
   int inputs; //may  need to be tweaked after we figure out how to take text
   // hashtable stores K -> V as interpreter -> code
   private Hashtable<String, String> codeTable = new Hashtable<String, String>();
@@ -19,6 +19,14 @@ public class Command{
     this.name = name;
   }
   //subroutines-----------------------------------------------------------------
+  //getters/setters
+  String getId()           { return id; }
+  void setId(String i)     { id = i; }
+  String getName()         { return name; }
+  void setName(String n)   { name = n; }
+  String getTooltip()      { return tooltip; }
+  void setTooltip(String t){ tooltip = t; return; }
+
   /* addLang
     Adds an entry to the hashtable (interp -> code) */
   void addLang(String interp, String code){codeTable.add(interp, code); return;}
--- src/ScriptStruct.java
+++ src/ScriptStruct.java
@@ -10,11 +10,20 @@ import java.util.ArrayList;
 
 public class ScriptStruct{
   //variables-------------------------------------------------------------------
-  private ArrayList<Command> flow;
+  ArrayList<Command> flow;
+  /*Set up the 'commands' hashtable.
+    'commands' holds one instance of each command, set up at program start */
+  Hashtable<String, Command> commands = new Hashtable<String, Command>();
   //constructors----------------------------------------------------------------
   public ScriptStruct(){
     flow = new ArrayList<Command>();
   }
   //subroutines-----------------------------------------------------------------
+  /* generateScript
+    Returns a (multi-line) string
+  */
+  boolean generateScript(Hashtable<String, Command> commands){
+
+  }
   //static subroutines----------------------------------------------------------
 }
