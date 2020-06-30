06b6ffd069d1c83d2153609493326ba6b3aaaa44
rflandau <rflandau@pm.me>
2020-06-29 23:03:06 -0700

    Started framework so we can actually start other tasks.

---
+++ src/Coding_To_End_Coding.java
@@ -0,0 +1,19 @@
+import java.util.Hashtable;
+
+/* Coding
+  The main java file. */
+
+public class Coding_To_End_Coding{
+  /* main
+    The main method.
+    @params  -
+    @returns -  */
+  public static void main(String[] args){
+
+    //NYI
+    //System.out.println("Retrieving commands from ./commands/");
+    //Hashtable<String, Command> commands = generateCommandList();
+
+    System.exit(0);
+  }
+}
