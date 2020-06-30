aecfd69481f360174a5da3c37e2d4329b0332f52
rflandau <rflandau@pm.me>
2020-06-30 09:49:05 -0700

    Added JFX lib. Added compile/run scripts.

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script does not rely on PATH_TO_FX being defined.
+# However, if you compile w/o this script, you probably ought to define that:
+# export PATH_TO_FX=path/to/javafx-sdk-14/lib
+#
+# Also! If you need more modules, add them to this script, unless they are
+#   already included transitively (just fuck around with it and figure out if it
+#   is needed or not).
+jfx_path=lib/javafx-sdk-14.0.1/lib
+javac -d classes/ \
+  --module-path  $jfx_path \
+  --add-modules javafx.controls \
+  src/*.java
