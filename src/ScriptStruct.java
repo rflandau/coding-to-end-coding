caff23ee73ebbf9a648768288828889a7a5d6570
rflandau <rflandau@pm.me>
2020-06-30 15:31:44 -0700

    Set up AL flow and HT commandBases as the internal representations.

--- src/ScriptStruct.java
+++ src/ScriptStruct.java
@@ -3,55 +3,60 @@ import java.util.Hashtable;
 import java.lang.StringBuilder;
 /* ScriptStruct
   Represents the main generation class for storing the flowchart and outputting
-  to the selected out-lang.
-  It is mostly a wrapper for the ArrayList 'flow', but is useful for organizing
-    subroutines.
-  'Flow' holds references to the commands stored in the hashtable in main.
-  Declaration ("new" tag)should NOT be used with relation to flow or we will add
-    a significant amount of overhead. */
+  to the selected out-lang. */
 
 public class ScriptStruct{
   //variables-------------------------------------------------------------------
+  /*'Flow' holds command representations of the GUI flowchart. */
   ArrayList<Command> flow;
+
   /*Set up the 'commands' hashtable.
-    'commands' holds one instance of each command, set up at program start */
-  Hashtable<String, Command> commands;
+    'commands' holds one instance of each command, set up at program start.
+    It should be assembed with addCommandBase to prevent duplicates. */
+  Hashtable<String, Command> commandBases;
   //constructors----------------------------------------------------------------
   public ScriptStruct(){
     flow = new ArrayList<Command>();
-    commands = new Hashtable<String, Command>();
+    commandBases = new Hashtable<String, Command>();
   }
   //subroutines-----------------------------------------------------------------
   /* addCommand
-    A wrapper for Hashtable.put that wards duplicates.
+    A wrapper for Hashtable.put() that wards duplicates.
     Returns false on failure. */
-  boolean addCommand(String id, Command c){
+  boolean addCommandBase(String id, Command c){
     boolean toReturn = false;
     //if key already exists, fail
-    if(commands.containsKey(id)){
+    if(commandBases.containsKey(id)){
       System.err.println("ERROR: " + id + " is already a key in commands and " +
                           "was not added.");
       toReturn = false;
-    }else{
-      commands.put(id, c);
-      toReturn = true;
-    }
+    }else{ commandBases.put(id, c); toReturn = true; }
+    //return status
     return toReturn;
+  }
+  /* addCommandToFlow
+  */
+  void addCommandToFlow(){
+    //NYI
   }
   /* generateScript
     Returns a (multi-line) string of the completed script, built from the items
     in 'flow'.
     Must be passed an interpreter that matches one of the interpreters in
       command.codeTable or the program will error.
-    Uses StringBuilder to build the script efficiently. */
+    Uses StringBuilder to build the script efficiently.
+    NYI: Uses helper function */
   String generateScript(String interp){
     StringBuilder script = new StringBuilder(100); //MAGIC NUMBER, arbitrary
-    //print interp
-    script.append(interp);
+    //add interpreter to top of script, plus newline
+    script.append(interp + "\n\n");
     //iterate through every element in 'flow'
     for(int i = 0; i<flow.size(); i++){
+      Command c = flow.get(i);
 
     }
+    //ensure the script is end-capped by a newline
+    script.append("\n");
     return script.toString();
   }
   //static subroutines----------------------------------------------------------
