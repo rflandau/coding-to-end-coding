98f2ec814aafee63590703a9969ace1522ad2cf7
rflandau <rflandau@pm.me>
2020-06-30 12:52:46 -0700

    Fixed documentation

--- src/ScriptStruct.java
+++ src/ScriptStruct.java
@@ -2,20 +2,19 @@ import java.util.ArrayList;
 /* ScriptStruct
   Represents the main generation class for storing the flowchart and outputting
   to the selected out-lang.
+  It is mostly a wrapper for the ArrayList 'flow', but is useful for organizing
+    subroutines.
   'Flow' holds references to the commands stored in the hashtable in main.
   Declaration ("new" tag)should NOT be used with relation to flow or we will add
     a significant amount of overhead. */
 
 public class ScriptStruct{
   //variables-------------------------------------------------------------------
-  private ArrayList<Command> flow = new ArrayList<Command>();
+  private ArrayList<Command> flow;
   //constructors----------------------------------------------------------------
   public ScriptStruct(){
-
+    flow = new ArrayList<Command>();
   }
   //subroutines-----------------------------------------------------------------
-  void insertCommand(Command c, int i){
-
-  }
   //static subroutines----------------------------------------------------------
 }
