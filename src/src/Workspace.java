d41d3b82450071782d4bf6508597dacd1109e9f4
Max <max.nibler@gmail.com>
2020-07-01 11:52:48 -0700

    added event handler but it is not linked to an object in the scene. Created a main canvas object but it has not been added to the scene

--- src/src/Workspace.java
+++ src/src/Workspace.java
@@ -7,6 +7,11 @@ import javafx.scene.shape.Rectangle;	//for sidebar shape
 import javafx.scene.paint.Color;		//for sidebar color
 import javafx.scene.layout.VBox;		//for vertical ordering of sidebar objects
 import javafx.geometry.Insets;			//for sidebar spacing
+
+//Import Event Handling
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
 //import javafx.scene.image.Image;
 //I know import .* is gross, but I had to import, like, 6 things to get the background
 //import javafx.scene.layout.*;
@@ -15,53 +20,73 @@ import javafx.stage.Stage;
 import prefabs.ExportButton;
 
 public class Workspace extends Application {
-	//hard-coded window sizes, can be changed later
-	double defaultWindowWidth = 800;
-	double defaultWindowHeight = 600;
+    //hard-coded window sizes, can be changed later
+    double defaultWindowWidth = 800;
+    double defaultWindowHeight = 600;
 	
-	//Applications do not need constructors
-	//However, the program arguments from launch can be accessed with getParameters()
-	
-	/*
-	init is called right before start, before the application comes into being
-	Stuff for its parts shouldn't be made here, but anything that needs to be
-	prepared for the starting of the app that isn't JavaFX can go here
-	The superclass definition also does nothing, so I commented this out
-	@Override
-	public void init() {
+    //Applications do not need constructors
+    //However, the program arguments from launch can be accessed with getParameters()
+    
+    /*
+      init is called right before start, before the application comes into being
+      Stuff for its parts shouldn't be made here, but anything that needs to be
+      prepared for the starting of the app that isn't JavaFX can go here
+      The superclass definition also does nothing, so I commented this out
+      @Override
+      public void init() {
 		
-	}
-	*/
-	//"/resources/images/WorkspaceBackgroundTile.png"
-	@Override
-	public void start(Stage stage) throws Exception {
+      }
+    */
+    //"/resources/images/WorkspaceBackgroundTile.png"
+    @Override
+    public void start(Stage stage) throws Exception {
 		
-		//scene (window) is created here
-	    stage.setTitle("My JavaFX Application");
-	    Group root = new Group();
-	    Scene scene = new Scene(root, defaultWindowWidth, defaultWindowHeight);
-	    stage.setScene(scene);
+	//scene (window) is created here
+	stage.setTitle("My JavaFX Application");
+	Group root = new Group();
+	Scene scene = new Scene(root, defaultWindowWidth, defaultWindowHeight);
+	stage.setScene(scene);
         
-	    //making all the containers for visual elements
-	    //VBoxes vertically order things. sidebar is for the sidebar, but NOT its contents
-	    VBox sidebar = new VBox(10);	//this number spaces the elements
-	    sidebar.setPadding(new Insets(10));	//this number puts a buffer around the box
-	    ExportButton button = new ExportButton(150, 100);
-	    Rectangle sidebarRect = new Rectangle(
-        		150, 
-        		defaultWindowHeight - button.getHeight(), 
-        		Color.LIGHTGREY
-						  );
-	    sidebar.getChildren().addAll(button, sidebarRect);
+	//making all the containers for visual elements
+	//VBoxes vertically order things. sidebar is for the sidebar, but NOT its contents
+	VBox sidebar = new VBox(10);	//this number spaces the elements
+	sidebar.setPadding(new Insets(10));	//this number puts a buffer around the box
+	ExportButton button = new ExportButton(150, 100);
+	Rectangle sidebarRect = new Rectangle(
+					      150, 
+					      defaultWindowHeight - button.getHeight(), 
+					      Color.LIGHTGREY
+					      );
+	sidebar.getChildren().addAll(button, sidebarRect);
         
-	    //adding objects to the scene
-	    root.getChildren().add(sidebar);
-		
-	    //show scene
-	    stage.show();
+	//adding objects to the scene
+	root.getChildren().add(sidebar);
 
-	}
-        
+	//Creating Event handler for click to add block
+	
+	EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+	    @Override
+	    public void handle(MouseEvent e){
+		System.out.println("Mouse click handled");
+	    }
+	};
+
+	// Creating the main canvas for the command blocks to be placed
+	Rectangle mainCanvas = new Rectangle(
+					     300,
+					     defaultWindowHeight,
+					     Color.LIGHTGREY
+					     );
+
+	
+		
+	//show scene
+	stage.show();
+
+    }
+
+
+    
 
     
 	/*
