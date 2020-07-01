151fbd5615a1a409bc4cfdb1a3027c593d8e55d5
Max <max.nibler@gmail.com>
2020-07-01 11:28:24 -0700

    adding file for me to test mouse events. Could be integrated into other file later if needed

---
+++ src/src/EventHandling.java
@@ -0,0 +1,10 @@
+package src;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.shape.Rectangle;	//for sidebar shape
+import javafx.scene.paint.Color;		//for sidebar color
+import javafx.scene.layout.VBox;		//for vertical ordering of sidebar objects
+import javafx.geometry.Insets;	
+import javafx.stage.Stage;		//for sidebar spacing
