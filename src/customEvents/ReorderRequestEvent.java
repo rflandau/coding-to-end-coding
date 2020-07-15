ce0f429ad6bfeb4f7a075c27c8c32c35eec25776
wil <wijohnso@ucsc.edu>
2020-07-15 13:25:19 -0700

    ReorderRequestEvent.java review pass eleventythree

--- src/customEvents/ReorderRequestEvent.java
+++ src/customEvents/ReorderRequestEvent.java
@@ -4,19 +4,25 @@ import javafx.event.Event;
 import javafx.event.EventType;
 import prefabs.CommandBlock;
 
+/*
+    ReorderRequestEvent
+    NOTE: finish these comments
+*/
 public class ReorderRequestEvent extends Event{
-    //publicly declaring this event's type
-    public static EventType<ReorderRequestEvent> VSPReorderEvent = new EventType<ReorderRequestEvent>("VSPReorderEvent");  
-    //idk what this is, but because events are serializable, Eclipse whined at me until I put it here
-    private static final long serialVersionUID = 3397303991642114951L;
-    
-    //this event carries its source, 
-    //and the x/y position of the object the source is being reordered relative to (the guest)
-    CommandBlock source;
-    double guestX;
-    double guestY;
+    //variables----------------------------------------------------------------
+    public static EventType<ReorderRequestEvent> VSPReorderEvent =
+        new EventType<ReorderRequestEvent>("VSPReorderEvent");  // event type
+    private static final long serialVersionUID = 3397303991642114951L; // ???
+    CommandBlock    source;     // event source
+    double          guestX;     // relative X position of element reordering to
+    double          guestY;     // relative Y position of element reordering to
 
-    public ReorderRequestEvent(CommandBlock requestee, double draggedX, double draggedY) {
+    //constructors-------------------------------------------------------------
+    /*
+        constructor that generates new CommandBlocks for the anchors
+    */
+    public ReorderRequestEvent(CommandBlock requestee, double draggedX,
+        double draggedY) {
         //VSPEvent is for events related to the VerticalSortingPane class
         super(VSPReorderEvent);
         
@@ -25,14 +31,26 @@ public class ReorderRequestEvent extends Event{
         this.guestY = draggedY - CommandBlock.height;
     }
     
+    /*
+        getSource()
+        NOTE: finish these comments
+    */
     public CommandBlock getSource(){
         return source;
     }
     
+    /*
+        getGuestX()
+        NOTE: finish these comments
+    */
     public double getGuestX() {
         return guestX;
     }
     
+    /*
+        getGuestY()
+        NOTE: finish these comments
+    */
     public double getGuestY() {
         return guestY;
     }
